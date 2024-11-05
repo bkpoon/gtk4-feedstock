f38e947cdafdf5eef32046958d33c1d0bc880f5d
Billy K. Poon <bkpoon@lbl.gov>
2024-11-05 11:48:04 -0800

    Print error message instead of failing in post-link.sh
    This is needed for cross-compiling because the error will cause the build to fail

--- recipe/post-link.sh
+++ recipe/post-link.sh
@@ -1,3 +1,21 @@
-"${PREFIX}/bin/glib-compile-schemas" "${PREFIX}/share/glib-2.0/schemas"
-"${PREFIX}/bin/gtk4-update-icon-cache" -f -t "${PREFIX}/share/icons/hicolor"
-"${PREFIX}/bin/gio-querymodules" "${PREFIX}/lib/gtk-4.0/4.0.0/printbackends"
+# When cross-compiling, or installing for a different platform, package binaries cannot be executed
+# See similar issue with gdk-pixbuf
+# https://github.com/conda-forge/gdk-pixbuf-feedstock/issues/23
+"${PREFIX}/bin/glib-compile-schemas" "${PREFIX}/share/glib-2.0/schemas" 2>> "${PREFIX}/.messages.txt" || \
+(
+  echo "ERROR: Failed to run glib-compile-schemas"
+  echo "To fix this, activate the environment and run:"
+  echo "    glib-compile-schemas ${CONDA_PREFIX}/share/glib-2.0/schemas"
+) >> "${PREFIX}/.messages.txt"
+"${PREFIX}/bin/gtk4-update-icon-cache" -f -t "${PREFIX}/share/icons/hicolor" 2>> "${PREFIX}/.messages.txt" || \
+(
+  echo "ERROR: Failed to run gtk4-update-icon-cache"
+  echo "To fix this, activate the environment and run:"
+  echo "    gtk4-update-icon-cache -f -t ${CONDA_PREFIX}/share/icons/hicolor"
+) >> "${PREFIX}/.messages.txt"
+"${PREFIX}/bin/gio-querymodules" "${PREFIX}/lib/gtk-4.0/4.0.0/printbackends" 2>> "${PREFIX}/.messages.txt" || \
+(
+  echo "ERROR: Failed to run gio-querymodules"
+  echo "To fix this, activate the environment and run:"
+  echo "    gio-querymodules ${CONDA_PREFIX}/lib/gtk-4.0/4.0.0/printbackends"
+) >> "${PREFIX}/.messages.txt"
